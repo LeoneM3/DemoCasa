91047ac20cd647cf89da93918445f508a08cd158
LeoneM3 <73596072+LeoneM3@users.noreply.github.com>
2021-06-11 20:15:25 -0600

    control

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,14 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     Vector2 inputMov;
-    float velCamina = 10f;
+    Vector2 inputRot;
+    public float velCamina = 10f;
+    public float SenM = 1f;
+    Transform Cam;
+    float rotX;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        Cam = transform.GetChild(0);
+        rotX = Cam.eulerAngles.x;
     }
 
     // Update is called once per frame
@@ -22,6 +27,10 @@ public class PlayerController : MonoBehaviour
         //controles
         inputMov.x = Input.GetAxis("Horizontal");
         inputMov.y = Input.GetAxis("Vertical");
+
+        //camara
+        inputRot.x = Input.GetAxis("Mouse X") * SenM;
+        inputRot.y = Input.GetAxis("Mouse Y") * SenM;
     }
 
     private void FixedUpdate()
@@ -31,5 +40,18 @@ public class PlayerController : MonoBehaviour
             transform.forward * vel * inputMov.y
             + transform.right * vel * inputMov.x
             ;
+
+
+
+        //cambiar position con rotacion
+
+        transform.rotation *= Quaternion.Euler(0, inputRot.x , 0);
+
+        //mirar arriba y abajo
+
+        rotX -= inputRot.y;
+        rotX = Mathf.Clamp(rotX, -50, 50);
+        Cam.localRotation = Quaternion.Euler(rotX, 0, 0);
     }
-}
+} 
+ 
