92eb3828f627fc0ae4c9d08d795b1bcbcf583ec2
LeoneM3 <73596072+LeoneM3@users.noreply.github.com>
2021-06-14 12:19:47 -0600

    nuevo player

---
+++ Assets/nuevos Scripts/CameraLook.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+
+    public float mouseSen = 80f;
+
+    public Transform playerBody;
+
+    float xRotation = 0;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSen * Time.deltaTime;
+
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSen * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
