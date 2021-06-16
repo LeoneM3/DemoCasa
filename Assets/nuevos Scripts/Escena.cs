a2891141752a412b5c5c2274ed9936d2e6b3160d
LeoneM3 <73596072+LeoneM3@users.noreply.github.com>
2021-06-15 20:37:44 -0600

    c

---
+++ Assets/nuevos Scripts/Escena.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+ 
+
+
+public class Escena : MonoBehaviour
+{   
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
